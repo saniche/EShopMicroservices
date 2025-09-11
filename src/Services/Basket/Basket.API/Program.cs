16a2874637e8e0e619dba6518effcb15f2aea5fe
Cheick SANOGO <saniche1@hotmail.fr>
2025-09-10 22:00:19 -0400

    Basket API set up

---
+++ src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,19 @@
+using BuildingBlocks.Behaviors;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+
+var app = builder.Build();
+
+app.MapCarter();
+
+app.Run();
