562c17fbdf6dd74a44a1d7cb8d814a79c126fbc7
Cheick SANOGO <saniche1@hotmail.fr>
2025-10-09 22:06:17 -0400

    Ordering API Tested

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,6 +1,7 @@
 ﻿using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Carter;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ordering.API;
 
@@ -10,9 +11,9 @@ public static class DependencyInjection
     {
         services.AddCarter();
 
-        //services.AddExceptionHandler<CustomExceptionHandler>();
-        //services.AddHealthChecks()
-        //    .AddSqlServer(configuration.GetConnectionString("Database")!);
+        services.AddExceptionHandler<CustomExceptionHandler>();
+        services.AddHealthChecks()
+            .AddSqlServer(configuration.GetConnectionString("Database")!);
 
         return services;
     }
@@ -21,12 +22,12 @@ public static class DependencyInjection
     {
         app.MapCarter();
 
-        //app.UseExceptionHandler(options => { });
-        //app.UseHealthChecks("/health",
-        //    new HealthCheckOptions
-        //    {
-        //        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-        //    });
+        app.UseExceptionHandler(options => { });
+        app.UseHealthChecks("/health",
+            new HealthCheckOptions
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
 
         return app;
     }
